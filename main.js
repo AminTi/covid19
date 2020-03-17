6941b9e8b41fbabe7de064c9318e472cc1d79d7a
Amin Titi <titiamin@icloud.com>
2020-03-17 18:18:33 +0100

    co19

---
+++ main.js
@@ -0,0 +1,31 @@
+let select = document.getElementById("countries")
+
+async function getCountry(country) {
+    let res = await fetch(`https://covid19.mathdro.id/api/countries/${country}`)
+    return res.json()
+}
+
+
+select.addEventListener("change", function (e) {
+    let country = select.value
+    getCountry(country).then(data => {
+        updateUi(country, data)
+
+
+    })
+})
+
+
+function updateUi(country, data) {
+    let h2 = document.querySelector('h2')
+    h2.innerHTML = country
+
+    let list = document.querySelector('.list')
+    list.innerHTML = `
+    <li> Confiremd: ${data.confirmed.value}</li>
+    <li> Recoverd: ${data.recovered.value}</li>
+    <li> Deaths: ${data.deaths.value}</li>
+    <li> lastUpdate: ${data.lastUpdate}</li>
+   `
+
+}
